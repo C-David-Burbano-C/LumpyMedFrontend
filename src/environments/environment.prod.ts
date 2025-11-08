5b729ea192be92ad4e4743e136b876352ddbec8e
C-David-Burbano-C <david.burbano@simerelectronics.com>
2025-11-08 10:20:00 -0500

    Add environment configuration files

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://production-api.com/api',
+  tokenKey: 'pediatric_dosage_token',
+  userKey: 'pediatric_dosage_user'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:8080/api',
+  tokenKey: 'pediatric_dosage_token',
+  userKey: 'pediatric_dosage_user'
+};
