070a67375ee4a6ff203199b3baf5dea8d7ed7eb1
C-David-Burbano-C <david.burbano@simerelectronics.com>
2025-11-08 19:00:00 -0500

    Create calculator module structure

---
+++ src/app/modules/calculator/calculator.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+
+import { CalculatorComponent } from './calculator.component';
+
+const routes: Routes = [
+  { path: '', component: CalculatorComponent }
+];
+
+@NgModule({
+  declarations: [
+    CalculatorComponent
+  ],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    TranslateModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+    MatAutocompleteModule,
+    MatToolbarModule,
+    MatIconModule,
+    RouterModule.forChild(routes)
+  ]
+})
+export class CalculatorModule { }
